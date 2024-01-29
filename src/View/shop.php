cb06560f6ecf5a331f0fb103730031f97ca57aba
Axel <axel.vair@laplateforme.io>
2024-01-29 18:12:00 +0100

    show products

--- src/View/shop.php
+++ src/View/shop.php
@@ -4,6 +4,16 @@ require_once "../../vendor/autoload.php";
 use App\Model\Product;
 
 $product = new Product();
-print_r($product->findAll());
+$products = $product->findAll();
+
+foreach ($products as $getProduct){
+    echo "<pre>";
+    echo "Nom: " . $getProduct->getName() . " ";
+    echo "Prix: " . $getProduct->getPrice() . " ";
+    echo "Description: " . $getProduct->getDescription() . "";
+    echo "Quantité: " . $getProduct->getQuantity() . " ";
+    echo "Description: " . $getProduct->getDescription() . " ";
+    echo "</pre>";
+}
 
 ?>

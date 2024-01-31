24506737c052ccec1e32b999fcbf66dad90ef6d6
Axel <axel.vair@laplateforme.io>
2024-01-31 09:30:53 +0100

    add register

---
+++ src/View/register.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inscription</title>
+</head>
+<body>
+    <form method="POST">
+        <label for="fullname">
+            Nom complet
+            <input type="text" name="fullname"/>
+        </label>
+
+        <label for="email">
+            Email:
+            <input type="email" name="email"/>
+        </label>
+
+        <label for="password">
+            Mot de passe
+            <input type="password" name="password"/>
+        </label>
+    </form>
+</body>
+</html>

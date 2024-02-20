d18094452af139b4567e6b9300f164aebd23721f
Axel <axel.vair@laplateforme.io>
2024-02-20 10:29:05 +0100

    card on show product page

--- src/Route/register-route.php
+++ src/Route/register-route.php
@@ -14,6 +14,4 @@ $router->map('POST', '/register', function() {
 
     $registerController = new AuthenticationController();
     $response = $registerController->register($fullname, $email, $password);
-
-
-}, 'register');
+});
--- src/View/product-show.php
+++ src/View/product-show.php
@@ -1,8 +1,11 @@
 <?php
-
 use App\Model\User;
-
+if (!$_SESSION) {
+    header("location: login");
+}
 $_SESSION['user'] = User::isLoggedIn();
+require_once "src/Template/__header.html";
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,12 +13,17 @@ $_SESSION['user'] = User::isLoggedIn();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Details</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.2/zephyr/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.2/zephyr/bootstrap.rtl.min.css">
+
 </head>
 <body>
-<h1>Product Details</h1>
+<div class="d-flex justify-content-center container mt-2 mb-3">
+    <h2>Détails de <?= $productById->getName() ?></h2>
+</div>
 
-<section class="d-flex container">
-        <div class="card mb-3 w-25 mx-1">
+<section class="d-flex d-flex justify-content-center container">
+        <div class="card mb-3 w-50 mx-1">
             <h3 class="card-header"><?= $productById->getName() ?></h3>
             <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200"
                  aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice"
